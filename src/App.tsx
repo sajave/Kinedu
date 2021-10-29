a8dfbf850141e8d3b7af718ea2accfb9d543f49f
Santiago <santiagojavierveiga@gmail.com>
2021-10-29 11:12:21 -0300

    feat(client/App): button to test GET

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route} from "react-router-dom";
+import "./App.css";
+import {Areas} from "./components/areas/Areast";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Route exact path='/' component={Areas} />
     </div>
   );
 }
