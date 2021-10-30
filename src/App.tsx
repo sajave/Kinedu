c3b33a1b81de2b23d4e5cff8e86f10db6a1faaab
Santiago <santiagojavierveiga@gmail.com>
2021-10-30 16:43:16 -0300

    fix(client/index): spelling check of Areas component name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Route} from "react-router-dom";
 import "./App.css";
-import {Areas} from "./components/areas/Areast";
+import {Areas} from "./components/areas/Areas";
 
 function App() {
   return (
