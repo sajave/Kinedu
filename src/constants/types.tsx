011e8c93b774d436db5edad76fd5cc16fe1dc537
Santiago <santiagojavierveiga@gmail.com>
2021-10-30 16:47:48 -0300

    feat(client/types): typescript interfaces based on received data

--- src/constants/types.tsx
+++ src/constants/types.tsx
@@ -4,5 +4,36 @@ export interface action {
 }
 
 export interface rootState {
-  data: {};
+  physical: Skill;
+  socialAndEmotional: Skill;
+}
+
+export interface Skill {
+  age_range: string;
+  area_id: number;
+  children: boolean;
+  description: string;
+  id: number;
+  logo_mobile: string;
+  logo_web: string;
+  milestones: Array<milestone>;
+  parent_skill_id: null;
+  percentil_availability: boolean;
+  title: string;
+}
+
+export interface milestone {
+  id: number;
+  age: number;
+  skill_id: number;
+  master: boolean;
+  area_id: number;
+  title: string;
+  description: string;
+  science_fact: string;
+  source_data: string;
+  parent_skill_id: null;
+  media_jpg: string;
+  media_mp4: string;
+  answer: null;
 }
