e7172908a0c4f54ecaefde65d0f94e32771d6f1d
Santiago <santiagojavierveiga@gmail.com>
2021-10-30 16:46:43 -0300

    feat(client/actions): created action to fetch physical and social&emotional data

--- src/store-redux/actions/index.tsx
+++ src/store-redux/actions/index.tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-export const GET_ALL_DATA = "GET_ALL_DATA";
+export const GET_PHYSICAL = "GET_PHYSICAL";
+export const GET_SOCIAL_AND_EMOTIONAL = "GET_SOCIAL_AND_EMOTIONAL";
 
-export function getAllData() {
+export function getDataPhysycal() {
   return async function (dispatch: any) {
-    const allPegadores = await axios.get(
+    const dataPhysycal = await axios.get(
       "https://api.kinedu.com/v3/babies/7652729/skills/23/milestones",
       {
         headers: {
@@ -14,8 +15,26 @@ export function getAllData() {
       }
     );
     dispatch({
-      type: GET_ALL_DATA,
-      payload: allPegadores.data,
+      type: GET_PHYSICAL,
+      payload: dataPhysycal.data,
+    });
+  };
+}
+
+export function getSocialAndEmotional() {
+  return async function (dispatch: any) {
+    const socialAndEmotional = await axios.get(
+      "https://api.kinedu.com/v3/babies/7652729/skills/2/milestones",
+      {
+        headers: {
+          Authorization:
+            "Token token=ed31bae57f79e6b7835b866cdaa3c7e2bfb90b922d91f3882ca451e28f6948d06b67aa23407cadc24f77c46088d62e282d629f574aef0cab62179ec5119b4d70",
+        },
+      }
+    );
+    dispatch({
+      type: GET_SOCIAL_AND_EMOTIONAL,
+      payload: socialAndEmotional.data,
     });
   };
 }
