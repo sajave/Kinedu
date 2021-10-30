76447962156528b519ca319db606da643b5a8509
Santiago <santiagojavierveiga@gmail.com>
2021-10-30 16:44:47 -0300

    feat(client/reducer): add physical and socialAndEmotional states to reducer

--- src/store-redux/reducer/index.ts
+++ src/store-redux/reducer/index.ts
@@ -1,16 +1,78 @@
-import {GET_ALL_DATA} from "../actions/index";
+import {GET_PHYSICAL, GET_SOCIAL_AND_EMOTIONAL} from "../actions/index";
 import {action, rootState} from "../../constants/types";
 
 const initialState: rootState = {
-  data: {},
+  physical: {
+    age_range: "",
+    area_id: 0,
+    children: false,
+    description: "",
+    id: 0,
+    logo_mobile: "",
+    logo_web: "",
+    milestones: [
+      {
+        id: 0,
+        age: 0,
+        skill_id: 0,
+        master: false,
+        area_id: 0,
+        title: "",
+        description: "",
+        science_fact: "",
+        source_data: "",
+        parent_skill_id: null,
+        media_jpg: "",
+        media_mp4: "",
+        answer: null,
+      },
+    ],
+    parent_skill_id: null,
+    percentil_availability: false,
+    title: "",
+  },
+  socialAndEmotional: {
+    age_range: "",
+    area_id: 0,
+    children: false,
+    description: "",
+    id: 0,
+    logo_mobile: "",
+    logo_web: "",
+    milestones: [
+      {
+        id: 0,
+        age: 0,
+        skill_id: 0,
+        master: false,
+        area_id: 0,
+        title: "",
+        description: "",
+        science_fact: "",
+        source_data: "",
+        parent_skill_id: null,
+        media_jpg: "",
+        media_mp4: "",
+        answer: null,
+      },
+    ],
+    parent_skill_id: null,
+    percentil_availability: false,
+    title: "",
+  },
 };
 
 export default function rootReducer(state = initialState, action: action) {
   switch (action.type) {
-    case GET_ALL_DATA:
+    case GET_PHYSICAL:
       return {
         ...state,
-        data: action.payload,
+        physical: action.payload.data.skill,
+      };
+    case GET_SOCIAL_AND_EMOTIONAL:
+      return {
+        ...state,
+        socialAndEmotional: action.payload.data.skill,
       };
     default:
       return {...state};
