5438176db3105263e7915ea692b30de39390566c
Santiago <santiagojavierveiga@gmail.com>
2021-10-30 16:51:05 -0300

    feat(client/MilestonesList): component that maps and shows each milestone, with styles

---
+++ src/components/milestonesList/MilestonesList.tsx
@@ -0,0 +1,37 @@
+import {milestone} from "../../constants/types";
+import {MilestoneCard} from "../milestoneCard/MilestoneCard";
+import "./MilestonesList.css";
+
+type Props = {
+  milestones: milestone[];
+};
+
+export function MilestoneListComponent({milestones}: Props) {
+  console.log("milestones props", milestones);
+
+  return (
+    <div className='listOfMilestones'>
+      {milestones.map((e) => {
+        return (
+          <div key={e.id}>
+            <MilestoneCard
+              id={e.id}
+              age={e.age}
+              skill_id={e.skill_id}
+              master={e.master}
+              area_id={e.area_id}
+              title={e.title}
+              description={e.description}
+              science_fact={e.science_fact}
+              source_data={e.source_data}
+              parent_skill_id={e.parent_skill_id}
+              media_jpg={e.media_jpg}
+              media_mp4={e.media_mp4}
+              answer={e.answer}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
