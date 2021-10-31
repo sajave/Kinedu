cac0b7857b7f68b367071bbd3980aef29dec3758
Santiago <santiagojavierveiga@gmail.com>
2021-10-31 00:19:06 -0300

    fix(client/MilesttonStateButton): change styles based on status

--- src/components/milestoneStateButton/MilestoneStateButton.tsx
+++ src/components/milestoneStateButton/MilestoneStateButton.tsx
@@ -4,21 +4,36 @@ import "./MilestoneStateButton.css";
 export function MilestoneStateButton() {
   const [milestoneCurrentState, setMilestoneCurrentState] =
     useState("Not Answered");
+  const [milestoneCurrentStateStyle, setMilestoneCurrentStateStyle] = useState({
+    milestoneStateButtonContainer: "milestoneStateButtonContainerNA",
+    milestoneStateButton: "milestoneStateButtonNA",
+  });
 
   function changeMilestoneState() {
     if (milestoneCurrentState === "Uncompleted") {
       setMilestoneCurrentState("Completed");
+      setMilestoneCurrentStateStyle({
+        ...milestoneCurrentStateStyle,
+        milestoneStateButtonContainer: "milestoneStateButtonContainerCompleted",
+        milestoneStateButton: "milestoneStateButtonCompleted",
+      });
       return;
     } else {
       setMilestoneCurrentState("Uncompleted");
+      setMilestoneCurrentStateStyle({
+        ...milestoneCurrentStateStyle,
+        milestoneStateButtonContainer:
+          "milestoneStateButtonContainerUncompleted",
+        milestoneStateButton: "milestoneStateButtonUncompleted",
+      });
       return;
     }
   }
   return (
     <div>
-      <h3 id='milestoneStateButtonContainer'>
+      <h3 id={milestoneCurrentStateStyle.milestoneStateButtonContainer}>
         <button
-          id='milestoneStateButton'
+          id={milestoneCurrentStateStyle.milestoneStateButton}
           onClick={() => changeMilestoneState()}
         >
           {milestoneCurrentState}
