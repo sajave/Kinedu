6f1a1fc5728a5caef4414a75dbeaba07d28183bd
Santiago <santiagojavierveiga@gmail.com>
2021-11-03 12:24:35 -0300

    fix(client/actions): request to custom backend

--- src/store-redux/actions/index.tsx
+++ src/store-redux/actions/index.tsx
@@ -6,7 +6,7 @@ export const MILESTONE_STATE = "MILESTONE_STATE";
 
 export function getDataPhysical() {
   return async function (dispatch: any) {
-    const dataPhysical = await axios.get(
+    /* const dataPhysical = await axios.get(
       "https://api.kinedu.com/v3/babies/7652729/skills/23/milestones",
       {
         headers: {
@@ -14,6 +14,9 @@ export function getDataPhysical() {
             "Token token=ed31bae57f79e6b7835b866cdaa3c7e2bfb90b922d91f3882ca451e28f6948d06b67aa23407cadc24f77c46088d62e282d629f574aef0cab62179ec5119b4d70",
         },
       }
+    ); */
+    const dataPhysical = await axios.get(
+      "http://localhost:3002/endpointPhysical"
     );
     dispatch({
       type: GET_PHYSICAL,
@@ -24,7 +27,7 @@ export function getDataPhysical() {
 
 export function getSocialAndEmotional() {
   return async function (dispatch: any) {
-    const socialAndEmotional = await axios.get(
+    /* const socialAndEmotional = await axios.get(
       "https://api.kinedu.com/v3/babies/7652729/skills/2/milestones",
       {
         headers: {
@@ -32,6 +35,9 @@ export function getSocialAndEmotional() {
             "Token token=ed31bae57f79e6b7835b866cdaa3c7e2bfb90b922d91f3882ca451e28f6948d06b67aa23407cadc24f77c46088d62e282d629f574aef0cab62179ec5119b4d70",
         },
       }
+    ); */
+    const socialAndEmotional = await axios.get(
+      "http://localhost:3002/endpointSocial&Emotional"
     );
     dispatch({
       type: GET_SOCIAL_AND_EMOTIONAL,
